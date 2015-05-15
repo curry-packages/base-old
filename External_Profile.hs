39fdf114b4751fc83d57b69ee33a5ad4a4744423
Björn Peemöller <bjp@informatik.uni-kiel.de>
2015-05-15 13:17:38 +0200

    Extended profiling possibilities for KiCS2 (using GHC >= 7.2)

--- External_Profile.hs
+++ External_Profile.hs
@@ -1,12 +1,37 @@
-import System.Mem (performGC)
+{-# LANGUAGE CPP #-}
+
 import System.CPUTime
+import System.Mem (performGC)
+
+#if __GLASGOW_HASKELL__ > 702
+import GHC.Stats
+#endif
+
+-- #endimport - do not remove this line!
+
+instance ConvertCurryHaskell C_ProcessInfo C_ProcessInfo where
+  toCurry   = id
+  fromCurry = id
+
+getProcessInfos :: IO [(C_ProcessInfo, Int)]
+#if __GLASGOW_HASKELL__ > 702
+getProcessInfos = do
+  stats <- getGCStats
+  return [ (C_RunTime           , floor (mutatorCpuSeconds   stats * 1000))
+         , (C_ElapsedTime       , floor (mutatorWallSeconds  stats * 1000))
+         , (C_Heap              , fromIntegral (maxBytesUsed stats))
+         , (C_Memory            , fromIntegral (maxBytesUsed stats))
+         , (C_GarbageCollections, fromIntegral (numGcs       stats))
+         ]
+#else
+getProcessInfos = do
+  t <- getCPUTime
+  return [(C_RunTime, t `div` (10^9)]
+#endif
 
 external_d_C_getProcessInfos :: Cover -> ConstStore ->
    Curry_Prelude.C_IO (Curry_Prelude.OP_List (Curry_Prelude.OP_Tuple2 C_ProcessInfo Curry_Prelude.C_Int))
-external_d_C_getProcessInfos _ _ = fromIO $ do
-  t <- getCPUTime
-  return (Curry_Prelude.OP_Cons (Curry_Prelude.OP_Tuple2 C_RunTime (toCurry (t `div` (10^9))))
-                     Curry_Prelude.OP_List)
+external_d_C_getProcessInfos _ _ = toCurry getProcessInfos
 
 external_d_C_garbageCollectorOff :: Cover -> ConstStore -> Curry_Prelude.C_IO Curry_Prelude.OP_Unit
 external_d_C_garbageCollectorOff _ _ = toCurry (return () :: IO ()) -- not supported
