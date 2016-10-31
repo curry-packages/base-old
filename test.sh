44288de0e072f91244aaab0e18948f5e51f13b83
Michael Hanus <mh@informatik.uni-kiel.de>
2016-10-31 18:12:50 +0100

    test script adapted

--- test.sh
+++ test.sh
@@ -21,14 +21,14 @@ CCOPTS="-m100 -dInt"
 LOGFILE=xxx$$
 
 if [ $VERBOSE = yes ] ; then
-  $CURRYBIN/currycheck $CCOPTS $ALLTESTS
+  $CURRYBIN/curry check $CCOPTS $ALLTESTS
   if [ $? -gt 0 ] ; then
     exit 1
   fi
 else
-  $CURRYBIN/currycheck $CCOPTS $ALLTESTS > $LOGFILE 2>&1
+  $CURRYBIN/curry check $CCOPTS $ALLTESTS > $LOGFILE 2>&1
   if [ $? -gt 0 ] ; then
-    echo "ERROR in currycheck:"
+    echo "ERROR in curry check:"
     cat $LOGFILE
     exit 1
   fi
