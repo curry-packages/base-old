13f0b4abab7692425a2120e2d53285f92cfa2b7a
Jan Tikovsky <jrt@informatik.uni-kiel.de>
2017-11-13 12:19:23 +0100

    Use compiler name provided by Installation module

--- External_Distribution.hs
+++ External_Distribution.hs
@@ -1,7 +1,7 @@
 import qualified Installation as I
 
 external_d_C_curryCompiler :: Cover -> ConstStore -> Curry_Prelude.C_String
-external_d_C_curryCompiler _ _ = toCurry "kics2"
+external_d_C_curryCompiler _ _ = toCurry I.compilerName
 
 external_d_C_curryCompilerMajorVersion ::  Cover -> ConstStore -> Curry_Prelude.C_Int
 external_d_C_curryCompilerMajorVersion _ _ = toCurry I.majorVersion
